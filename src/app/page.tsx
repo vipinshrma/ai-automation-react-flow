8bffa6ffa807819e76a46f03e7b1aae9866fc46c
vipan sharma <vipansharma@Vipans-MacBook-Air.local>
2025-10-15 22:53:38 +0530

    update: add trpc provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+
+import { caller, trpc } from "@/trpc/server";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const users  = await caller.getUsers();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     <h1 className="text-sm text-center text-emerald-300">AI Automation</h1>
+     <h1 className="text-sm text-center">{
+     JSON.stringify(users)
+      }</h1>
     </div>
   );
 }
